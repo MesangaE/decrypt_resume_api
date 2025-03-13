52000790fa17792e6329fa3d24561023207ab2bf
Mesanga Ikome <102374235+MesangaE@users.noreply.github.com>
2025-03-13 15:24:40 +0000

    update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mesanga_resume"
+    bucket         = "mesanga-resume"
     key            = "terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "resume-state"
